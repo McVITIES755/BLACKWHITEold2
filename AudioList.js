9318316a7e77d645f7fb85357c5b84ffd5f27525
McVITIES755 <mcvities857@gmail.com>
2023-10-31 18:00:13 +0000

    Initial commit

---
+++ AudioList.js
@@ -0,0 +1,182 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Save Your Tears (Remix)",
+      artist: "The Weeknd and Ariana Grande",
+      img: "images/Save-Your-Tears.jpg",
+      src: "Save-Your-Tears"
+    },
+    {
+      name: "Akuma No Ko",
+      artist: "Ai Higuchi",
+      img: "images/Akuma-No-Ko.jpg",
+      src: "Akuma-No-Ko"
+    },
+    {
+      name: "Centuries",
+      artist: "Fall Out Boy",
+      img: "images/Centuries.jpg",
+      src: "Centuries"
+    },
+    {
+      name: "Cheap Thrills",
+      artist: "Sia and Sean Paul",
+      img: "images/Cheap-Thrills.jpg",
+      src: "Cheap-Thrills"
+    },
+    {
+      name: "Choix De Vie",
+      artist: "Randall and anas",
+      img: "images/Choix-De-Vie.jpg",
+      src: "Choix-De-Vie"
+    },
+    {
+      name: "Clean Up",
+      artist: "Chunkz and Yung Filly",
+      img: "images/Clean-Up.jpg",
+      src: "Clean-Up"
+    },
+    {
+      name: "Derniere Danse",
+      artist: "Indila",
+      img: "images/Derniere-Danse.jpg",
+      src: "Derniere-Danse"
+    },
+    {
+      name: "Feel This Moment",
+      artist: "Pitbull and Christina Aguilera",
+      img: "images/Feel-This-Moment.jpg",
+      src: "Feel-This-Moment"
+    },
+    {
+      name: "Habibi",
+      artist: "Ricky Rich",
+      img: "images/Habibi.jpg",
+      src: "Habibi"
+    },
+    {
+      name: "Hero",
+      artist: "Skillet",
+      img: "images/Hero.jpg",
+      src: "Hero"
+    },
+    {
+      name: "Hoshikuzu Venus",
+      artist: "Aimer",
+      img: "images/Hoshikuzu-Venus.jpg",
+      src: "Hoshikuzu-Venus"
+    },
+    {
+      name: "Kick Down The Door",
+      artist: "Harris J and AJ Jung",
+      img: "images/Kick-Down-The-Door.jpg",
+      src: "Kick-Down-The-Door"
+    },
+    {
+      name: "Leon On",
+      artist: "Major Lazer and DJ Snake",
+      img: "images/Lean-On.jpg",
+      src: "Lean-On"
+    },
+    {
+      name: "Let Me Love You",
+      artist: "DJ Snake and Justin Beiber",
+      img: "images/Let-Me-Love-You.jpg",
+      src: "Let-Me-Love-You"
+    },
+    {
+      name: "Locked Out Of Heaven",
+      artist: "Bruno Mars",
+      img: "images/Locked-Out-Of-Heaven.jpg",
+      src: "Locked-Out-Of-Heaven"
+    },
+    {
+      name: "Moves Like Jagger",
+      artist: "Maroon 5 and Christina Aguilera",
+      img: "images/Moves-Like-Jagger.jpg",
+      src: "Moves-Like-Jagger"
+    },
+    {
+      name: "One Dance",
+      artist: "Drake",
+      img: "images/One-Dance.jpg",
+      src: "One-Dance"
+    },
+    {
+      name: "Paro",
+      artist: "NEJ",
+      img: "images/Paro.jpg",
+      src: "Paro"
+    },
+    {
+      name: "Rewrite The Stars",
+      artist: "Anne Marie and James Arthur",
+      img: "images/Rewrite-The-Stars.jpg",
+      src: "Rewrite-The-Stars"
+    },
+    {
+      name: "See You Again",
+      artist: "Wiz Khalifa and Charlie Puth",
+      img: "images/See-You-Again.jpg",
+      src: "See-You-Again"
+    },
+    {
+      name: "Shock - Attack On Titan",
+      artist: "安藤裕子",
+      img: "images/Shock.jpg",
+      src: "Shock"
+    },
+    {
+      name: "Someone You Loved",
+      artist: "Lewis Capaldi",
+      img: "images/Someone-You-Loved.jpg",
+      src: "Someone-You-Loved"
+    },
+    {
+      name: "Taki Taki",
+      artist: "DJ Snake Selena Gomez Ozuna & Cardi B",
+      img: "images/TakiTaki.jpg",
+      src: "TakiTaki"
+    },
+    {
+      name: "Tourner Dans Le Vide",
+      artist: "Indila",
+      img: "images/Tourner-Dans-Le-Vide.jpg",
+      src: "Tourner-Dans-Le-Vide"
+    },
+    {
+      name: "Unravel",
+      artist: "TK from 凛として時雨",
+      img: "images/Unravel.jpg",
+      src: "Unravel"
+    },
+    {
+      name: "Wahran",
+      artist: "Randall",
+      img: "images/Wahran.jpg",
+      src: "Wahran"
+    },
+    {
+      name: "Starboy",
+      artist: "Weeknd and Daft Punk",
+      img: "images/Starboy.jpg",
+      src: "Starboy"
+    }
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+    // }
+  ];
